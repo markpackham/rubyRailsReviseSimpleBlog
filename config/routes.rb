10aa4d7672085733c18430b6e2b42d462f7c3255
mark <markpackham1@gmail.com>
2020-09-26 17:26:59 +0100

    show posts on index, https://youtu.be/pPy0GQJLZUM?t=2119

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-    root 'posts#index'
+    root 'posts#index', as: 'home_path'
 
-    get 'about' => 'pages#about'
+    get 'about_path' => 'pages#about', as: 'about'
+
+    get 'new_post_path' => 'posts#new'
 
     resources :posts
 end
